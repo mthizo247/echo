100d3555d0b51ad72ba44993b5f7fa83fbad7f7b
Ronald <mthizo247@gmail.com>
2026-01-19 05:51:22 +0200

    m

--- stop.sh
+++ stop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if sudo test -f "/echo.pid"; then
-  sudo kill -9 $(sudo cat /echo.pid)
+  sudo kill -9 $(sudo cat /echo.pid) || true
 fi
 
 if sudo test -f "/root/echo.pid"; then
-  sudo kill -9 $(sudo cat /root/echo.pid)
+  sudo kill -9 $(sudo cat /root/echo.pid) || true
 fi
 
 if sudo test -f "/home/mthizo247/echo.pid"; then
-  sudo kill -9 $(sudo cat /home/mthizo247/echo.pid)
+  sudo kill -9 $(sudo cat /home/mthizo247/echo.pid) || true
 fi
