27ad7f8ece2056f281981b9d83b809d32646ed21
Ronald <mthizo247@gmail.com>
2026-01-19 05:29:41 +0200

    m

--- stop.sh
+++ stop.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-sudo kill -9 $(cat /echo.pid) || true
-sudo kill -9 $(cat /root/echo.pid) || true
-sudo kill -9 $(cat /home/mthizo247/echo.pid) || true
+if [ -f "/echo.pid" ]; then
+  echo "/echo.pid exists"
+elif [ "/root/echo.pid" ]; then
+  echo "/root/echo.pid exist"
+elif [ "/home/mthizo247/echo.pid" ]; then
+  echo "/home/mthizo247/echo.pid exist"
+else
+  echo "Opps! file not found"
+fi
+
+#sudo kill -9 $(cat /echo.pid) || true
+#sudo kill -9 $(cat /root/echo.pid) || true
+#sudo kill -9 $(cat /home/mthizo247/echo.pid) || true
