7a2c13c310eab94b9ccb224bbf301a8dfc009636
Ronald <mthizo247@gmail.com>
2026-01-19 06:29:33 +0200

    m

--- stop.sh
+++ stop.sh
@@ -13,3 +13,7 @@ fi
 if sudo test -f "/home/mthizo247/echo.pid"; then
   sudo kill -9 $(sudo cat /home/mthizo247/echo.pid) || true
 fi
+
+if sudo test -f "/home/mthizo247/echo/echo.pid"; then
+  sudo kill -9 $(sudo cat /home/mthizo247/echo/echo.pid) || true
+fi
