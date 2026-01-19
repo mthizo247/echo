d198f9b7f2bef7c155c5da60aaa9d436e08312bb
Ronald <mthizo247@gmail.com>
2026-01-19 05:33:10 +0200

    m

--- stop.sh
+++ stop.sh
@@ -2,9 +2,9 @@
 
 if [ -f /echo.pid ]; then
   echo "/echo.pid exists"
-elif [ "/root/echo.pid" ]; then
+elif [ /root/echo.pid ]; then
   echo "/root/echo.pid exist"
-elif [ "/home/mthizo247/echo.pid" ]; then
+elif [ /home/mthizo247/echo.pid ]; then
   echo "/home/mthizo247/echo.pid exist"
 else
   echo "Opps! file not found"
